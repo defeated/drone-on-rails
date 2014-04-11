982edfea2b55bf0b13bdbd14ab009b7cbda5ba8c
eddie cianci <defeated2k4@gmail.com>
2014-04-11 00:33:32 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DroneOnRails::Application.config.secret_key_base = '13b080aafff714f395726fbf467e100c68a42ebababd6a7375f034151d8e5b857c578183fdf933bb996c43776b693d1362abf2b6f810d70e3b2a997e6b858793'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DroneOnRails::Application.config.session_store :cookie_store, key: '_drone-on-rails_session'
